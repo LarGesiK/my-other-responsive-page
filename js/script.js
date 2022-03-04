78f4a4f6c6deb3dde85059a40450cc3a00629fec
Daniel Góraj <larges3284@gmail.com>
2022-03-04 22:16:28 +0100

    added hover in section aboutus and autohide nav in mobile

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
-    
     const nav = document.querySelector('.navbar')
+    const allNavItems = document.querySelectorAll('.nav-link')
+    const navList = document.querySelector('.navbar-collapse')
+
 
     function addShadow() {
-
         if (window.scrollY >= 300) {
             nav.classList.add('shadow-bg')
         } else {
@@ -11,5 +12,6 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
+    allNavItems.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')))
     window.addEventListener('scroll', addShadow)
 })
